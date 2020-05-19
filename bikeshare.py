e49fabd0c3cd7a8de2e6ededa8ed021bd483a6c5
Esra Yılmaz <esrayilmaz308@gmail.com>
2020-05-19 14:41:03 +0300

    welcome message changed

--- bikeshare.py
+++ bikeshare.py
@@ -21,7 +21,7 @@ def get_filters():
         (str) month - name of the month to filter by, or "all" to apply no month filter
         (str) day - name of the day of week to filter by, or "all" to apply no day filter
     """
-    print('Hi there! Let\'s explore some US bikeshare data!')
+    print('Welcome! Let\'s explore some US bikeshare data!')
 
     # get specified city  
     while True:
